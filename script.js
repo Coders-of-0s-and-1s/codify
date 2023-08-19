ea86555dbd17c6216e6259f116a3bce4cf012370
MOULEE-SUDHARSAN-RY <22l234@psgtech.ac.in>
2023-08-19 13:50:50 +0530

    finished news section

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function myFunction(x) {
     x.classList.toggle("change");
   }
+let news='This webpage is decided to be published on september 3 as the event of "Nithish Kumar" birthday'
+document.getElementById("news").innerHTML=news;
