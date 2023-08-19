4a6a63bdf4e21133097016e4e114f9fec37e7ad1
MOULEE-SUDHARSAN-RY <22l234@psgtech.ac.in>
2023-08-19 16:57:40 +0530

    news scroll

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function myFunction(x) {
     x.classList.toggle("change");
   }
-let news='This webpage is decided to be published on september 3 as the event of "Nithish Kumar" birthday'
-document.getElementById("news").innerHTML=news;
+var i=0;
+let news=['This webpage is decided to be published on september 3 as the event of "Nithish Kumar" birthday','I appreciate evryone who involved in this project']
+document.getElementById("news").innerHTML=news[i];
+function nextnews(a){
+  i=i+a; 
+  if (i==news.length){
+    i=0;
+    console.log(i);
+  }
+  else if (i<0){
+    i=news.length -1;
+  } 
+  document.getElementById("news").innerHTML=news[i];
+}
+
