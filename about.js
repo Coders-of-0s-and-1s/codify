6dbd5dae091e2eadbe42edef8ecca79b01e3bee2
MOULEE-SUDHARSAN-RY <22l234@psgtech.ac.in>
2023-08-25 22:44:13 +0530

    done students zone for mobile and desktop

--- about.js
+++ about.js
@@ -1,4 +1,6 @@
-
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
 let about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, sapiente odit. Laboriosam iusto alias consequatur dicta, officia repellat quia ab illum fuga aspernatur provident id accusantium non possimus veniam quod?";
 document.getElementById("aboutb").innerHTML=about;
 // alert("Danger zone");
