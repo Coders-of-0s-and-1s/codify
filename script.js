5c50421a804b9a86352e9b280471f28059ad3dac
MOULEE SUDHARSAN R Y <129882046+MOULEE-SUDHARSAN-RY@users.noreply.github.com>
2023-09-07 06:56:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function myFunction(x) {
   }
 var i=0;
 let news=[];
-news.push("Radix by CVR happening tomorrow");
-news.push("NSS Inaguration tommorow @4.30pm Venue at F203, Be sure to check out");
+news.push("Radix by CVR happening today.");
+news.push("NSS Inaguration today @4.30pm Venue at F203, Be sure to check out");
 news.push("Tuesday 7th period is assinged as Analog Electronics.");
 document.getElementById("news").innerHTML=news[i];
 function nextnews(a){
