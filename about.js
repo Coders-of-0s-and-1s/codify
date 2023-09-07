8ac9ac9a5e290ee64b62c212ca3ac00dd5932a3f
MOULEE SUDHARSAN R Y <129882046+MOULEE-SUDHARSAN-RY@users.noreply.github.com>
2023-09-07 07:57:47 +0530

    Update about.js

--- about.js
+++ about.js
@@ -4,6 +4,6 @@ function myFunction(x) {
       mbtn.classList.toggle("change1");
     }
 
-    let about="During the semester exam time we can't find the study materials, notes, also we missed some of the classes and we didn't know the right syllabus sem exam and we struggled a lot, so in order to overcome all these problems we decided to make a web hub called 'CODIFY'. With the help of this website you can easily able to find all important links and materials. So be sure to use this website. Also provide your valuable feedback for our improvement. Thanks a lot for visiting this website...";
+    let about="During the semester exam time we can't find the study materials, notes, also we missed some of the classes and we didn't know the right syllabus  and so we struggled a lot, so in order to overcome all these problems we decided to make a web hub called 'CODIFY'. With the help of this website you can easily able to find all important links and materials. So be sure to use this website. Also provide your valuable feedback for our improvement. Thanks a lot for visiting this website...";
     document.getElementById("aboutb").innerHTML=about;
    alert("Danger zone press ok to continue");
