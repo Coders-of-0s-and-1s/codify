59a6cf0952d547bf5b31901e60fd6dd4b3652141
Harishb001 <141330706+Harishb001@users.noreply.github.com>
2023-09-08 07:36:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ var i=0;
 let news=[];
 news.push("Remainder : Maths homework due today");
 news.push("Tuesday 7th period is assinged as Analog Electronics.");
-news.push("Assessment tutorials for LANA and EMF on 13.08.2023");
+news.push("Assessment tutorials for LANA and EMF on 13.09.2023");
+news.push("Assessment tutorial for DSA on 14.09.2023");
 document.getElementById("news").innerHTML=news[i];
 function nextnews(a){
   i=i+a; 
