a712e306f4fcd118a49f5bff94369fe937b8cc5f
MOULEE SUDHARSAN R Y <129882046+MOULEE-SUDHARSAN-RY@users.noreply.github.com>
2023-09-09 08:22:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function myFunction(x) {
   }
 var i=0;
 let news=[];
+news.push("
+G20 Summit kicks off today, Full schedule as world leaders meet at Delhi's Bharat Mandapam");
 news.push("Tuesday 7th period is assinged as Analog Electronics.");
 news.push("Assessment tutorials of LANA and EMF on 13.09.2023");
 news.push("Assessment tutorial of DSA on 14.09.2023 at 8.30am");
